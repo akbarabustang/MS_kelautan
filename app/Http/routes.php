5c1271f156ea7377ba43a3c7c2a5b51490b11b36
lutfi <sainteclutfi@gmail.com>
2016-01-03 07:05:16 +0800

    master
    route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,20 +9,32 @@ Route::get('/app/layout', function () {
     return view('app.layout.main');
 });
 
-Route::get('/app/layout/master/pembudidaya', function () {
-    return view('app.master.pembudidaya');
+Route::get('/app/pembudidaya', function () {
+    return view('app.pembudidaya');
 });
 
-Route::get('/app/layout/master/nelayan', function () {
-    return view('app.master.nelayan');
+Route::get('/app/nelayan', function () {
+    return view('app.nelayan');
 });
 
-Route::get('/app/layout/master/pengolah', function () {
-    return view('app.master.pengolah');
+Route::get('/app/pengolah', function () {
+    return view('app.pengolah');
 });
 
-Route::get('/app/master', function () {
-    return view('app.master');
+Route::get('/app/master/bantuan', function () {
+    return view('app.master.bantuan');
+});
+
+Route::get('/app/master/jabatan', function () {
+    return view('app.master.jabatan');
+});
+
+Route::get('/app/master/jenis-usaha', function () {
+    return view('app.master.jenis-usaha');
+});
+
+Route::get('/app/master/sarana-pembudidaya', function () {
+    return view('app.master.sarana-pembudidaya');
 });
 
 Route::get('/app/pengaturan', function () {
